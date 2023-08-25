297633552b14e5dde56fa24319313e979ce94fdd
Anushka Jain <98316408+anushkaj1712@users.noreply.github.com>
2023-08-25 13:16:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header'; // Correct path to Header.js
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <p>Welcome to my Dex Screener App!</p>
+    </div>
+  );
+};
+
+export default App;
